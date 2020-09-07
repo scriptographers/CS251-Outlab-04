6abb3c9271106cd0b2f1697df2832ef5cb8f451c
devansh-dvj <devansh.dvj@gmail.com>
2020-09-07 23:00:21 +0530

    Task 2: Updated task2.py and tested on docker

--- task-2/task2.py
+++ task-2/task2.py
@@ -15,17 +15,16 @@ def lbl(algo):
         return '{} with epsilon={}'.format(*algo)
 
 
-if __name__ == '__main__':
-    data = pd.read_csv(args.data)
-    instances = data.groupby('instance')
-    for ist_name, data_ins in instances:
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.set_xlabel('horizon')
-        ax.set_ylabel('Regret')
-        ax.set_title('Instance {} - both axes in log scale'.format(ist_name[2]))
-        algorithms = data_ins.groupby(['algorithm', 'epsilon'])
-        for algo_name, data_algo in algorithms:
-            ax.loglog(data_algo.groupby('horizon')['REG'].mean(), label=lbl(algo_name))
-        plt.legend()
-        plt.savefig('instance{}.png'.format(ist_name[2]))
+data = pd.read_csv(args.data)
+instances = data.groupby('instance')
+for ist_name, data_ins in instances:
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('horizon')
+    ax.set_ylabel('Regret')
+    ax.set_title('Instance {} - both axes in log scale'.format(ist_name[2]))
+    algorithms = data_ins.groupby(['algorithm', 'epsilon'])
+    for algo_name, data_algo in algorithms:
+        ax.loglog(data_algo.groupby('horizon')['REG'].mean(), label=lbl(algo_name))
+    plt.legend()
+    plt.savefig('instance{}.png'.format(ist_name[2]))
