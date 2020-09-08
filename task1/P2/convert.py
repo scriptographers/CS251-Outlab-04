4ceee3cbe9140de5715e1e0b12564d20cf87671c
devansh-dvj <devansh.dvj@gmail.com>
2020-09-08 22:14:09 +0530

    Task 1: Tested on docker and fixed a minor bug

--- task1/P2/convert.py
+++ task1/P2/convert.py
@@ -3,23 +3,23 @@ import numpy as np
 # Constants
 DATA_PATH = "mumbai_data.csv"
 SAVE_PATH = "transformed.csv"
-POPULATION = 20.4 # Population of Mumbai (in Millions)
+POPULATION = 20.4  # Population of Mumbai (in Millions)
 
 # Preproccessing
 data = np.genfromtxt(DATA_PATH, dtype=None, delimiter=',', encoding="utf-8")
-header = data[0].astype("U50") # Unicode 50 character string
+header = data[0].astype("U50")  # Unicode 50 character string
 data = data[1:]
 days = data[:, 0]
 X = data[:, 1:].astype("float32")
 
 # Computation
-X[:,1] /= X[:,0] # Test positivity rate
-X[:,0] /= POPULATION # Tests per Million
-X = np.around(X, 3) # Round off to same no. of digits
+X[:, 1] /= X[:, 0]  # Test positivity rate
+X[:, 0] /= POPULATION  # Tests per Million
+X = np.around(X, 3)  # Round off to same no. of digits
 
 # Formatting
 header[1] = "Tests per Million"
-header[2] = "Test Positivity"
+header[2] = "Test Positivity rate"
 table = np.hstack((days[:, None], X))
 table = np.vstack((header, table))
 
--- task1/P3/merge.py
+++ task1/P3/merge.py
@@ -3,26 +3,26 @@ import numpy as np
 # Constants
 DATA1_PATH = "mumbai_data.csv"
 DATA2_PATH = "mumbai_unlock.csv"
-SAVE_PATH  = "info_combine.csv"
+SAVE_PATH = "info_combine.csv"
 
 # Preproccessing
-data1 = np.loadtxt(DATA1_PATH, skiprows=1, usecols = (1, 2), delimiter=',', encoding="utf-8")
-data2 = np.loadtxt(DATA2_PATH, skiprows=1, usecols = (1, 2), delimiter=',', encoding="utf-8")
-days  = np.loadtxt(DATA1_PATH, skiprows=1, dtype=str, usecols = (0), delimiter=',', encoding="utf-8")
+data1 = np.loadtxt(DATA1_PATH, skiprows=1, usecols=(1, 2), delimiter=',', encoding="utf-8")
+data2 = np.loadtxt(DATA2_PATH, skiprows=1, usecols=(1, 2), delimiter=',', encoding="utf-8")
+days = np.loadtxt(DATA1_PATH, skiprows=1, dtype=str, usecols=(0), delimiter=',', encoding="utf-8")
 
 # Computations
-infected_lock   = data1[:,1]
-infected_unlock = data2[:,1]
-tests_lock      = data1[:,0]
-tests_unlock    = data2[:,0]
-pos_lock        = infected_lock/tests_lock
-pos_unlock      = infected_unlock/tests_unlock
+infected_lock = data1[:, 1]
+infected_unlock = data2[:, 1]
+tests_lock = data1[:, 0]
+tests_unlock = data2[:, 0]
+pos_lock = infected_lock / tests_lock
+pos_unlock = infected_unlock / tests_unlock
 
 # Formatting
-numeric = np.around(np.column_stack((infected_unlock, infected_lock, pos_lock, pos_unlock)), 3) # Round off till 3 decimal places
-table   = np.column_stack((days, numeric))
-header  = np.asarray(["Day", "Infected(Unlock)", "Infected(Lock)", "Positivity Rate(Lock)", "Positivity Rate(Unlock)"], dtype=str)
-table   = np.vstack((header, table))
+numeric = np.around(np.column_stack((infected_unlock, infected_lock, pos_lock, pos_unlock)), 3)  # Round off till 3 decimal places
+table = np.column_stack((days, numeric))
+header = np.asarray(["Day", "Infected(Unlock)", "Infected(Lock)", "Positivity Rate(Lock)", "Positivity Rate(Unlock)"], dtype=str)
+table = np.vstack((header, table))
 
 # Saving as CSV
 np.savetxt(SAVE_PATH, table, fmt="%s", delimiter=',', encoding="utf-8")
