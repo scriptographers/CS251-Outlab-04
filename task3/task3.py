af72e12da728ea394ead869d28f7077cc93d2fa3
devansh-dvj <devansh.dvj@gmail.com>
2020-09-08 23:01:00 +0530

    Task 3: Tested on docker - slow

--- task3/task3.py
+++ task3/task3.py
@@ -1,5 +1,5 @@
 import argparse
-import numpy as np 
+import numpy as np
 from PIL import Image
 from scipy.cluster.vq import kmeans2
 
@@ -15,17 +15,17 @@ K = args.k
 
 # Pre-processing
 inp_img = Image.open(INP_PATH)
-inp_img = np.array(inp_img).astype(float) # Convert to numpy array
+inp_img = np.array(inp_img).astype(float)  # Convert to numpy array
 SHAPE = inp_img.shape
-inp_img = inp_img.reshape(-1, SHAPE[-1]) # Retain last dimension (color channels) , flatten others.
+inp_img = inp_img.reshape(-1, SHAPE[-1])  # Retain last dimension (color channels) , flatten others.
 
 # KMeans++ and constructing the output image
 centroids, labels = kmeans2(inp_img, K, minit="++")
-out_img = centroids[labels] # Index everything at once :)
+out_img = centroids[labels]  # Index everything at once :)
 
 # Post-processing
 out_img = out_img.reshape(SHAPE)
-out_img = np.round(out_img).astype('uint8') # Back to int
+out_img = np.round(out_img).astype('uint8')  # Back to int
 
 # Saving to output
 out_img = Image.fromarray(out_img)
