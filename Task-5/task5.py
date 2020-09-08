60a6a5ff2a72a66beeff26f2450d494ee064ebd1
Harshit Varma <varmaharshit2@gmail.com>
2020-09-08 10:04:09 +0530

    Task-5 completed

--- Task-5/task5.py
+++ Task-5/task5.py
@@ -9,7 +9,9 @@ DATA_URL   = "https://api.covid19india.org/csv/latest/case_time_series.csv"
 DATA_PATH  = "data.csv"
 PLOT_PATH  = "covid.png"
 START_DATE = "01 April " # Extra space at the end is important
+DPI        = 200 # Controls the quality of the saved image
 
+# Using requests to fetch data
 def getData(DATA_URL, DATA_PATH):
     req = requests.get(DATA_URL)
     content = req.content
@@ -17,48 +19,42 @@ def getData(DATA_URL, DATA_PATH):
     with open(DATA_PATH, "wb") as f:
         f.write(content)
 
-getData(DATA_URL, DATA_PATH)
-
 # Preprocessing
+def preprocess(df):
+    dates = df["Date"]
+    X = df["Total Deceased"]
+    idx_start = np.where(dates == START_DATE)[0][0]
+    X = X[idx_start:].to_numpy() # Required data
+    return X
+
+# Main
+
+getData(DATA_URL, DATA_PATH) # Call this once per day and then for subsequent uses comment this out
 df = pd.read_csv(DATA_PATH)
-dates = df["Date"]
-X = df["Total Deceased"]
-idx_start = np.where(dates == START_DATE)[0][0]
-X = X[idx_start:].to_numpy() # Required data
-N = len(X)
+X  = preprocess(df)
+N  = len(X)
 
-# Constructing H(t): To be improved
-H = np.zeros(N-1)
-for i in range(N-1):
-    H[i] = X[i+1]/X[i]
+# Constructing H(t):
+H = X[1:]/X[:-1] # We can also use np.roll(X, -1)/X, but this is faster
 
-t = np.linspace(1,N-1,N-1)
+t = np.linspace(1,N-1,N-1) # The time axis
 
 # Linear Regression
 inferences = LR(t, H)
-slope = inferences[0]
-intercept = inferences[1]
-fitted_line = slope*t + intercept
+m = inferences[0] # slope
+c = inferences[1] # intercept
+y_hat = m*t + c # fitted line
 
-# Finding the approximate day where the lewitt's metric is close to 1
-# eps = 10**(-3) # (y-eps,y+eps)
-# idx_stop = np.where(abs(fitted_line - 1) < eps)[0][0]
-# days_from_start = t[idx_stop]
-# print(int(days_from_start))
-days_from_start = round((1 - intercept)/slope) # y = 1 at the end
+# Finding the approximate day where the lewitt's metric is 1
+days_from_start = round((1 - c)/m) # y_hat = 1 at the end
 print(days_from_start)
 
 # Plotting
-plt.plot(t, H, label="H(t)")
-plt.plot(t, fitted_line, label="Fitted line")
+plt.plot(t, H, linewidth=0.5, label="H(t)")
+plt.plot(t, y_hat, linewidth=0.5, label="Fitted line")
+plt.hlines(1, t[0], t[-1], colors='black', linestyles='--', linewidth=0.5, label="H(t)=1")
 plt.xlabel("Time (days from April 01, 2020)")
 plt.ylabel("H(t)")
 plt.legend(loc='best')
 plt.title("Lewitt's Metric")
-plt.savefig(PLOT_PATH, dpi=300)
-
-
-
-
-
-
+plt.savefig(PLOT_PATH, dpi=DPI)
