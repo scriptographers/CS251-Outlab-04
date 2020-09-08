4d9700bfaf23e6ea8d7c41154df092257af3ef14
Harshit Varma <varmaharshit2@gmail.com>
2020-09-08 16:04:52 +0530

    Better printing method added

--- Task-1/analysis.py
+++ Task-1/analysis.py
@@ -1,12 +1,17 @@
 import numpy as np
 
 DATA_PATH = "mumbai_data.csv"
-np.set_printoptions(precision=3) # Since 3 decimal places is asked
 
 data = np.genfromtxt(DATA_PATH, dtype=None, delimiter=',', encoding='utf-8')
-headers = data[0][1:] # All except 'Days'
+header = data[0][1:] # All except 'Days'
+header = header[:, None]
 data = data[1:]
 numeric_data = data[:, 1:].astype('float32')
-means  = np.mean(numeric_data, axis=0)
-sigmas = np.std(numeric_data, axis=0)
-print(headers)
+means  = np.around(np.mean(numeric_data, axis=0)[:, None], 3)
+sigmas = np.around(np.std(numeric_data, axis=0)[:, None], 3)
+table  = np.hstack((header, means, sigmas))
+new_header = np.array(["Field", "Mean", "Std. Dev."])
+table = np.vstack((new_header, table))
+
+for row in table:
+    print(' '.join(row))
