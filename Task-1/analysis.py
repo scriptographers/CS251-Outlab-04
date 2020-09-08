4a203fd1e3049e5bf9b4192e0ec11c5de2b119b1
Harshit Varma <varmaharshit2@gmail.com>
2020-09-08 16:30:59 +0530

    Print formatting completed

--- Task-1/analysis.py
+++ Task-1/analysis.py
@@ -2,16 +2,22 @@ import numpy as np
 
 DATA_PATH = "mumbai_data.csv"
 
+# Preproccessing
 data = np.genfromtxt(DATA_PATH, dtype=None, delimiter=',', encoding='utf-8')
 header = data[0][1:] # All except 'Days'
 header = header[:, None]
 data = data[1:]
 numeric_data = data[:, 1:].astype('float32')
+
+# Computation
 means  = np.around(np.mean(numeric_data, axis=0)[:, None], 3)
 sigmas = np.around(np.std(numeric_data, axis=0)[:, None], 3)
+
+# Formatting
 table  = np.hstack((header, means, sigmas))
 new_header = np.array(["Field", "Mean", "Std. Dev."])
 table = np.vstack((new_header, table))
-
+padding = len(max(table.ravel(), key=len))
+padding = "{:" + str(padding) + "}" # Adds right padding to values
 for row in table:
-    print(' '.join(row))
+    print(' '.join(padding.format(val) for val in row))
