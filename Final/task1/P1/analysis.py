e7c5d369f7523b2649b7b1b5f1f40c781dc3fa92
Harshit Varma <varmaharshit2@gmail.com>
2020-09-13 19:09:56 +0530

    Updated the final repo with T1 P1 (Typo in the previous commit: T1 P1 instead of T1 P3)

--- Final/task1/P1/analysis.py
+++ Final/task1/P1/analysis.py
@@ -13,6 +13,13 @@ numeric_data = data[:, 1:].astype('float32')
 means = np.around(np.mean(numeric_data, axis=0)[:, None], 3)
 sigmas = np.around(np.std(numeric_data, axis=0)[:, None], 3)
 
+def format_floats(val):
+    try: 
+        float(val)
+        return format(float(val), ".3f")
+    except ValueError: 
+        return val 
+
 # Formatting
 table = np.hstack((header, means, sigmas))
 new_header = np.array(["Field", "Mean", "Std. Dev."])
@@ -20,4 +27,4 @@ table = np.vstack((new_header, table))
 padding = len(max(table.ravel(), key=len))
 padding = "{:" + str(padding) + "}"  # Adds right padding to values
 for row in table:
-    print(' '.join(padding.format(val) for val in row))
+    print(' '.join(padding.format(format_floats(val)) for val in row))
